71c423449ea7a40115796bc2c41f015bbded177a
EunjoungAhn <aej9300@gmail.com>
2023-01-25 22:09:13 +0900

    json형식으로 바꾸어 클래스 만들기

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,15 +1,24 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:webtoonapi/models/webtoon_model.dart';
 
 class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodaysToons() async {
+  // future를 기다리는 비동기 함수이기에
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      //print(response.body); String으로 보였지만, json형식이기에 변환해준다.
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon)); // 웹툰 클래스 정의
+      }
+      return webtoonInstances;
     }
     throw Error();
   }
