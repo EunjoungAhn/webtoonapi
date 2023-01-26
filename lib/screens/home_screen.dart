046458649f703ef4ae856adadd5397604520b1c6
EunjoungAhn <aej9300@gmail.com>
2023-01-26 19:52:40 +0900

    statelesswidget 상태에서 fetch 할 수 있다. FutureBuilder를 사용하면 된다.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,35 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:webtoonapi/models/webtoon_model.dart';
 import 'package:webtoonapi/services/api_service.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-// state는 UI와 변할 수 있는 데이터를 가지고 있다.
-class _HomeScreenState extends State<HomeScreen> {
-  List<WebtoonModel> webtoons = [];
-  bool isLoading = true;
-
-//http 요청을 기다리는함수를 기다려야 하기 때문에 async
-  void waitForWebToons() async {
-    webtoons = await ApiService.getTodaysToons();
-    isLoading = false;
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    waitForWebToons();
-  }
+  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
-    print(webtoons);
-    print(isLoading);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -41,6 +19,16 @@ class _HomeScreenState extends State<HomeScreen> {
           "오늘의 웹툰",
           style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
         ),
+      ),
+      body: FutureBuilder(
+        future: webtoons, // 값을 기다려줄 것이고,
+        builder: (context, snapshot) {
+          //snapshot을 통해서 변화를 알려줄 것이다.
+          if (snapshot.hasData) {
+            return const Text("There is data!");
+          }
+          return const Text("Loading....");
+        },
       ),
     );
   }
