db79bc572b26d18afc9cedb74036e44a8aceccd6
EunjoungAhn <aej9300@gmail.com>
2023-01-30 19:20:34 +0900

    hero wiget 사용하여 새로운 화면으로 덮는게 아닌 사진으로 이동하는 애니메이션 처럼 보이도록 처리

--- lib/widgets/webtoon_widget.dart
+++ lib/widgets/webtoon_widget.dart
@@ -26,19 +26,22 @@ class Webtoon extends StatelessWidget {
       },
       child: Column(
         children: [
-          Container(
-            width: 250,
-            // clipBehavior 자식의 부모 영역 침범을 제어하는 방법
-            clipBehavior: Clip.hardEdge,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: [
-                  BoxShadow(
-                      blurRadius: 15,
-                      offset: const Offset(10, 10), //  그림자의 위치
-                      color: Colors.black.withOpacity(0.5))
-                ]),
-            child: Image.network(thumb),
+          Hero(
+            tag: id,
+            child: Container(
+              width: 250,
+              // clipBehavior 자식의 부모 영역 침범을 제어하는 방법
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                        blurRadius: 15,
+                        offset: const Offset(10, 10), //  그림자의 위치
+                        color: Colors.black.withOpacity(0.5))
+                  ]),
+              child: Image.network(thumb),
+            ),
           ),
           const SizedBox(
             height: 10,
